2c2b59dd886f225b4a0dd5e24626d402c0fab5c7
olavogregorio <olavogregorio12@gmail.com>
2020-05-25 21:28:43 -0300

    atualização dos exercicios

---
+++ arrow-function.js
@@ -0,0 +1,18 @@
+// var ehPar = (n1) => {
+//     return n1 % 2 === 0
+// }
+// console.log(ehPar(2))
+// console.log(ehPar(3))
+
+var teste = {
+    nome: 'Olavo',
+    creatAnonFunction: function(){
+        console.log(this.nome)
+    }
+
+    // createAarrowFunction: function(){
+    //     return() => {
+    //         console.log(this.nome)
+    //     }
+}
+console.log(teste)
