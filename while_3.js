89d879f247c45a0d25e4d1930e71301de5d3f6f7
olavogregorio <olavogregorio12@gmail.com>
2020-05-25 21:35:16 -0300

    atualização

--- while_3.js
+++ while_3.js
@@ -1,15 +1,8 @@
-var rs = require('readline-sync')
-
-var num1 = rs.questionInt('qual o numero 1: ');
-
-
 var cont = 0;
-while (cont <= num1){
+while (cont <= 100){
     var resto = cont %2
-    if(resto == 0 ){
+    if(resto == 1 ){
         console.log(cont)
     }
     cont++;
 }
-
-// console.log(num1)
---
+++ while_4.js
@@ -0,0 +1,15 @@
+var rs = require('readline-sync')
+
+var num1 = rs.questionInt('qual o numero 1: ');
+
+
+var cont = 0;
+while (cont <= num1){
+    var resto = cont %2
+    if(resto == 0 ){
+        console.log(cont)
+    }
+    cont++;
+}
+
+// console.log(num1)
